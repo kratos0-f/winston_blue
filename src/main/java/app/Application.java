e6849a305cc707bb202e74cdaf0a60850caea8c4
kratos0-f <protopovichevgeny0@gmail.com>
2023-01-19 00:13:07 +0300

    java doc

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,11 +4,18 @@ import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
+/**
+ * Класс окна приложения
+ */
 public class Application implements Consumer<Event> {
-    // окно приложения
+    /**
+     * окно приложения
+     */
     private final Window window;
 
-    // конструктор приложения
+    /**
+     * Конструктор окна приложения
+     */
     public Application() {
         // создаём окно
         window = App.makeWindow();
@@ -18,14 +25,18 @@ public class Application implements Consumer<Event> {
         window.setVisible(true);
     }
 
-    // обработчик событий
+    /**
+     * Обработчик событий
+     *
+     * @param e событие
+     */
     @Override
     public void accept(Event e) {
         // если событие - это закрытие окна
         if (e instanceof EventWindowClose) {
             // завершаем работу приложения
             App.terminate();
-        }else if (e instanceof EventWindowCloseRequest) {
+        } else if (e instanceof EventWindowCloseRequest) {
             window.close();
         }
     }
