941f5b40ac52b487cf7fbc5d27c0de4d3fa64618
kratos0-f <protopovichevgeny0@gmail.com>
2023-02-01 22:52:08 +0300

    window title ready

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -21,6 +21,12 @@ public class Application implements Consumer<Event> {
         window = App.makeWindow();
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
+        // задаем заголовок окна
+        window.setTitle("Java 2D");
+        // задаём размер окна
+        window.setWindowSize(900, 900);
+        // задаём его положение
+        window.setWindowPosition(100, 100);
         // делаем окно видимым
         window.setVisible(true);
     }
